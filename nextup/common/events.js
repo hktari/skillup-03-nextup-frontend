f46c513a8ab7d65c7f331c3ed65729c4f2473417
hktari <bkamnik1995@gmail.com>
2022-10-26 08:14:08 +0200

    implement event api calls

--- nextup/common/events.js
+++ nextup/common/events.js
@@ -1,11 +1,59 @@
 import axios from './http'
 
-function getFeatured(){
-    return axios.get('/event')
+// GET /event/featured
+function getFeatured() {
+    return axios.get('/event/featured')
+}
+
+
+// GET /event
+function all(startIdx, pageSize) {
+    return axios.get('/event/', {
+        params: {
+            startIdx,
+            pageSize
+        }
+    })
+}
+
+// POST /event (auth guard)
+function create(event) {
+    return axios.post('/event', event)
+}
+
+// DELETE /event (auth guard)
+function remove(eventId) {
+    return axios.delete('/event/' + eventId)
+}
+
+// PUT /event (auth guard)
+function update(event) {
+    return axios.put('/event/' + eventId, event)
+}
+
+// GET /event/search (?date&location)
+function search(date, location) {
+    return axios.get('/event/search', {
+        params: {
+            date,
+            location
+        }
+    })
+}
+
+// GET /event/{id}
+function getDetails(eventId) {
+    return axios.get('/event/' + eventId)
 }
 
 const eventsApi = {
-    getFeatured
+    getFeatured,
+    all,
+    create,
+    remove,
+    update,
+    search,
+    getDetails,
 }
 
 export default eventsApi
