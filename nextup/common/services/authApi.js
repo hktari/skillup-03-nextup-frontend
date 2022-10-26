4aa4a8139012c4238626ee714116bf5aec4ec486
hktari <bkamnik1995@gmail.com>
2022-10-26 08:39:49 +0200

    add location api calls and password reset api calls

--- nextup/common/services/authApi.js
+++ nextup/common/services/authApi.js
@@ -20,17 +20,32 @@ function signup(email, firstName, lastName, password, imageBase64) {
         })
 }
 
-// password-reset
+// POST /auth/password-reset
 function requestPasswordReset(email) {
     return axios.post('/auth/password-reset', {
         email
     })
 }
 
+// GET /auth/password-reset/:token
+function validatePasswordResetToken(token) {
+    return axios.get('/auth/password-reset/' + token)
+}
+
+// POST /auth/password-reset/:token
+function performPasswordReset(token, password) {
+    return axios.post('/auth/password-reset/' + token, {
+        password
+    })
+}
+
+
 const authApi = {
     login,
     signup,
-    requestPasswordReset
+    requestPasswordReset,
+    validatePasswordResetToken,
+    performPasswordReset
 }
 
 export default authApi
---
+++ nextup/common/services/locationApi.js
@@ -0,0 +1,16 @@
+import axios from './http'
+
+// GET /location/autocomplete?text={}
+function search(text) {
+    return axios.get('/location/autocomplete', {
+        params: {
+            text
+        }
+    })
+}
+
+const locationApi = {
+    search
+}
+
+export default locationApi
