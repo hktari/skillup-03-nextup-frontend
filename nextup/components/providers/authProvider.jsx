ceb096985f0b56ef047fcdccc390172b51d026b7
hktari <bkamnik1995@gmail.com>
2022-10-28 11:00:51 +0200

    auth provider import fix

--- nextup/components/providers/authProvider.jsx
+++ nextup/components/providers/authProvider.jsx
@@ -4,7 +4,7 @@ import { useContext, useState, createContext } from "react";
 const AuthContext = createContext({ state: {}, actions: {} });
 
 
-export function AutfhProvider({ children }) {
+export default function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
 
     async function login(email, password) {
@@ -38,12 +38,11 @@ export function useAuth() {
     return useContext(AuthContext)
 }
 
-
 // export function RequireAuth({ children }) {
-//     let { state: { user } } = useAuth();
-//     let location = useLocation();
+//     let auth = useAuth();
+//     // let location = useLocation();
 
-//     if (!user) {
+//     if (!auth.isLoggedIn()) {
 //         // Redirect them to the /login page, but save the current location they were
 //         // trying to go to when they were redirected. This allows us to send them
 //         // along to that page after they login, which is a nicer user experience
@@ -53,4 +52,6 @@ export function useAuth() {
 
 //     return children;
 // }
+
+
 
--- nextup/pages/_app.jsx
+++ nextup/pages/_app.jsx
@@ -4,21 +4,13 @@ import '../styles/globals.css'
 import Layout from '../components/layout/layout';
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { AuthProvider } from '../components/providers/authProvider';
+import AuthProvider from '../components/providers/authProvider';
+
 function MyApp({ Component, pageProps }) {
-
-  const router = useRouter()
-
-  useEffect(() => {
-    console.log(router.pathname)
-    if (router.pathname.replace('/', '').trim() === '') {
-      console.log('root path')
-      router.replace('/landing-page')
-    }
-  }, [router.pathname])
 
   // Use the layout defined at the page level, if available
   const specialLayout = Component.getLayout
+
 
   return (
     <AuthProvider>
